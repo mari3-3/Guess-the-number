cf1a5944daae71d7d16fa255f3c10c7b65f8f2d1
mari3-3 <0somethingsomething0@gmail.com>
2024-10-30 08:20:59 -0400

    Create version3.py

---
+++ version3.py
@@ -0,0 +1,27 @@
+import random
+
+newton = random.randint(1, 10)
+print(newton)
+
+print("We are going to play a game called {Guess the Number!}")
+print()
+print("Please guess any number from 1-10!!")
+print()
+number = int(input("Type a number!:"))
+
+counter = 1
+
+while counter <= 2:
+ if number == newton:
+    print()
+    print("You got it!")
+    break
+ else:
+    print()
+    print("Wrong;p")
+    print()
+    number = int(input("Try again siily:"))
+    counter += 1
+if counter == 3:
+    print()
+    print("YOU LOSEEEEE >:P")
